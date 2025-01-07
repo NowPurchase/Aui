214ca46e4232fa2e4bb0005c41445bacb710832b
user <user@users-MacBook-Air.local>
2025-01-07 15:10:50 +0530

    Implement ref forwarding

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./style.scss";
 import { Icons, iconMap } from "iconConstants";
 
-export type Variants = "primary" | "secondary" | "outlined" | "tertiary" | "icon" | "text";
-export type Direction = "ltr" | 'rtl' | "top-to-bottom" | "bottom-to-top";
-export type fontWeight = 'light' | 'normal' | 'semi-bold' | 'bold'; // Named font weights
+export type Variants =
+  | "primary"
+  | "secondary"
+  | "outlined"
+  | "tertiary"
+  | "icon"
+  | "text";
+export type Direction = "ltr" | "rtl" | "top-to-bottom" | "bottom-to-top";
+export type fontWeight = "light" | "normal" | "semi-bold" | "bold"; // Named font weights
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -13,41 +19,46 @@ export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   direction?: Direction;
   weight?: fontWeight;
   text?: React.ReactNode;
-  active?: Boolean
+  active?: Boolean;
 }
 
-const Button: React.FC<Props> = ({
-  children,
-  icon,
-  text = "",
-  variant = "primary",
-  className = "",
-  direction = 'ltr',
-  weight = 'light',
-  active = false,
-  ...props
-}) => {
+// Forward the ref to the underlying button element
+const Button = forwardRef<HTMLButtonElement, Props>(
+  (
+    {
+      children,
+      icon,
+      text = "",
+      variant = "primary",
+      className = "",
+      direction = "ltr",
+      weight = "light",
+      active = false,
+      ...props
+    },
+    ref
+  ) => {
+    // Map the icon prop to a Remix icon class
+    const getIconClass = () => {
+      return iconMap[icon as Icons] || "";
+    };
 
-  // Map the icon prop to a Remix icon class
-  const getIconClass = () => {
-    return iconMap[icon as Icons] || '';
-  };
+    // Map fontWeight to actual pixel value
+    const fontWeightClass = weight ? `font-weight-${weight}` : "";
 
-  // Map fontWeight to actual pixel value
-  const fontWeightClass = weight ? `font-weight-${weight}` : '';
-
-
-  return (
-    <button
-      data-variant={variant}
-      data-active={active}
-      className={`btn dir-${direction} ${icon && 'btn-with-icon'} aui-btn-${variant} ${fontWeightClass} ${className}`}
-      {...props}
-    >
-      {icon && <i className={`${getIconClass()}`} />}
-      {children || text}
-    </button>
-  );
-};
+    return (
+      <button
+        ref={ref}
+        data-variant={variant}
+        data-active={active}
+        className={`btn dir-${direction} ${icon && "btn-with-icon"} aui-btn-${variant} ${fontWeightClass} ${className}`}
+        {...props}
+      >
+        {icon && <i className={`${getIconClass()}`} />}
+        {children || text}
+      </button>
+    );
+  }
+);
 
 export default Button;
